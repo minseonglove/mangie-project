13d1d33cd0232f8fd09674a002b13647ee971f0a
minseonglove <qnfpdlr124@naver.com>
2021-12-30 02:54:19 +0900

    SongList.java 추가 커스텀 서열표 기능 일부 개발

---
+++ mangie-project/src/main/java/com/mangieproject/CustomBoard/SongList.java
@@ -0,0 +1,48 @@
+package com.mangieproject.CustomBoard;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.util.Arrays;
+
+@RestController
+public class SongList {
+
+    @GetMapping("/songlist")
+    public String[][] getList(){
+        File f = new File("mangie-vue/src/assets/img/thumbnails");
+        // dlc폴더 배열
+        String[] dlcList = f.list();
+        int dirLen = dlcList.length;
+        // dlc별로 곡 이름을 넣을 배열
+        String[][] songList = new String[dirLen+1][];
+        // 곡 갯수
+        int songCount = 0;
+        for(int i = 1; i <= dirLen; i++){
+            f = new File("mangie-vue/src/assets/img/thumbnails/"+dlcList[i-1]);
+            // 폴더 안에 든 곡 리스트를 가져와서 저장
+            String[] list = f.list();
+            int len = list.length;
+            // .jpg 때기
+            for(int k = 0; k < len; k++)
+                list[k] = list[k].split("\\.")[0];
+            songList[i] = new String[list.length];
+            songList[i] = list.clone();
+            songCount += list.length;
+        }
+        // 전체 곡 이름을 넣을 0번 배열 크기 할당
+        songList[0] = new String[songCount];
+        songCount = 0;
+        for(int i = 1; i <= dirLen; i++){
+            // 야무지게 넣는다
+            for(String s : songList[i]){
+                songList[0][songCount] = s;
+                songCount++;
+            }
+        }
+        Arrays.sort(songList[0]);
+        System.out.println("songList를 가져왔어여");
+        return songList;
+    }
+}
--- mangie-project/src/main/java/com/mangieproject/MangieProjectApplication.java
+++ mangie-project/src/main/java/com/mangieproject/MangieProjectApplication.java
@@ -6,15 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 public class MangieProjectApplication {
 
     public static void main(String[] args) {
-    /*    File[] list = new File("mangie-vue/src/assets/img/thumbnails").listFiles();
-        for(File f : list){
-            System.out.println(f.getAbsolutePath());
-        }*/
         SpringApplication.run(MangieProjectApplication.class, args);
     }
 
